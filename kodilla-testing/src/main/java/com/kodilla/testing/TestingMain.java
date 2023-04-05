3e6a6daebc16a1841a8f80967a2a49f4e9e3e99a
Wojciech Mysliwiec <wojmys@gmail.com>
2023-04-05 15:48:53 +0200

    6.2 Added Calculator Class

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+
+//        Test1
+        int result1 =calculator.add(2,5);
+
+        if(result1==7){
+            System.out.println("add() test OK");
+        } else {
+            System.out.println("add() test FAILED");
+        }
+
+//        Test2
+        int result2 = calculator.subtract(100,99);
+        if(result2==1){
+            System.out.println("substract() test OK");
+        } else {
+            System.out.println("substract() test FAILED");
+        }
+
+        }
+
+
+
+
+
+
     }
-}
+
