fe5a4039697621ff6cb80f054770f0a01756635d
Wojciech Mysliwiec <wojmys@gmail.com>
2023-04-15 09:12:32 +0200

    7.2 added ForumUser Class

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/iterate/StreamMain.java
@@ -0,0 +1,9 @@
+package com.kodilla.stream.iterate;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+    }
+}
