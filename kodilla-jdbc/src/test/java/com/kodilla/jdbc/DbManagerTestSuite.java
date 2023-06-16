bef22138193ee726deb363b7864d825543d4c226
Wojciech Mysliwiec <wojmys@gmail.com>
2023-06-16 11:18:14 +0200

    test select users deleted

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -44,36 +44,6 @@ public class DbManagerTestSuite {
         assertEquals(5, counter);
     }
 
-    @Test
-    void testSelectUsersAndPosts() throws SQLException {
-        //given
-        DbManager dbManager = DbManager.getInstance();
-
-        //when
-        String sqlQuery = """
-                SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POSTS_NUMBER
-                FROM USERS U
-                         JOIN POSTS P ON U.ID=P.USER_ID
-                GROUP BY U.ID
-                HAVING COUNT(*)>=2;
-                """;
-        Statement statement = dbManager.getConnection().createStatement();
-        ResultSet rs = statement.executeQuery(sqlQuery);
-
-        //then
-        int counter = 0;
-        while (rs.next()) {
-            System.out.println(
-                    rs.getString("FIRSTNAME") + ", " +
-                            rs.getString("LASTNAME") + ", " +
-                            rs.getInt("POSTS_NUMBER")
-            );
-            counter++;
-        }
-        rs.close();
-        statement.close();
-        assertEquals(1, counter);
-    }
 }
 
 
