5015f9158ba918f14cd3c049d667815b116138c5
Wojciech Mysliwiec <wojmys@gmail.com>
2023-06-21 13:07:19 +0200

    Entity created

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -0,0 +1,32 @@
+package com.kodilla.hibernate.task.dao;
+
+import com.kodilla.hibernate.task.Task;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TaskDaoTestSuite {
+
+    private TaskDao taskDao;
+    private static final String DESCRIPTION = "Test: Learn Hibernate";
+
+    @Test
+    void testTaskDaoSave() {
+        //Given
+        Task task = new Task(DESCRIPTION, 7);
+
+        //When
+        taskDao.save(task);
+
+        //Then
+        int id = task.getId();
+        Optional<Task> readTask = taskDao.findById(id);
+        assertTrue(readTask.isPresent());
+
+        //CleanUp
+        taskDao.deleteById(id);
+    }
+}
