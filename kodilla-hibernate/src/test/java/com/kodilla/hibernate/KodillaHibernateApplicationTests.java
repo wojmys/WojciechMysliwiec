dd468516129b8c79f39867b08075db0f21a36495
Wojciech Mysliwiec <wojmys@gmail.com>
2023-06-21 08:47:41 +0200

    17.1 kodilla-hibernate module added

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kodilla.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaHibernateApplicationTests {
+
+//    Test ten sprawdza, czy można nawiązać połączenie
+//    z bazą danych.
+
+    @Test
+    void contextLoads() {
+    }
+
+}
