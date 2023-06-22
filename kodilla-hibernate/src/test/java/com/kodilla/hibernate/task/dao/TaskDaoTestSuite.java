947dadadfe13572c4a53847d1c66c1c291a8f5b6
Wojciech Mysliwiec <wojmys@gmail.com>
2023-06-22 14:56:53 +0200

    TaskDaOTestSuide added

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -1,10 +1,12 @@
 package com.kodilla.hibernate.task.dao;
 
 import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +17,9 @@ class TaskDaoTestSuite {
 
     @Autowired
     private TaskDao taskDao;
+    @Autowired
+    private TaskListDao taskListDao;
+
     private static final String DESCRIPTION = "Test: Learn Hibernate";
 
     @Test
@@ -51,5 +56,48 @@ class TaskDaoTestSuite {
         //CleanUp
         int id = readTasks.get(0).getId();
         taskDao.deleteById(id);
+    }
+    @Test
+    void testTaskDaoSaveWithFinancialDetails(){
+
+        //given
+        Task task = new Task(DESCRIPTION,30);
+        task.setTaskFinancialDetails(new TaskFinancialDetails(new BigDecimal(120), false));
+
+        //when
+        taskDao.save(task);
+        int id=task.getId();
+
+        //then
+        assertNotEquals(0,id);
+
+        //CleanUp
+        taskDao.deleteById(id);
+
+    }
+    @Test
+    void testTaskListDaoSaveWithTasks() {
+        //Given
+        Task task = new Task("Test: Learn Hibernate", 14);
+        Task task2 = new Task("Test: Write some entities", 3);
+        TaskFinancialDetails tfd = new TaskFinancialDetails(new BigDecimal(20), false);
+        TaskFinancialDetails tfd2 = new TaskFinancialDetails(new BigDecimal(10), false);
+        task.setTaskFinancialDetails(tfd);
+        task2.setTaskFinancialDetails(tfd2);
+        TaskList taskList = new TaskList("LISTNAME", "ToDo tasks");
+        taskList.getTasks().add(task);
+        taskList.getTasks().add(task2);
+        task.setTaskList(taskList);
+        task2.setTaskList(taskList);
+
+        //When
+        taskListDao.save(taskList);
+        int id = taskList.getId();
+
+        //Then
+        assertNotEquals(0, id);
+
+        //CleanUp
+        taskListDao.deleteById(id);
     }
 }
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskFinancialDetailsDaoTestSuite.java
@@ -0,0 +1,38 @@
+package com.kodilla.hibernate.task.dao;
+
+import com.kodilla.hibernate.task.TaskFinancialDetails;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class TaskFinancialDetailsDaoTestSuite {
+
+    @Autowired
+    private TaskFinancialDetailsDao taskFinancialDetailsDao;
+
+    @Test
+    void testFindByPaid() {
+
+        //given
+
+        TaskFinancialDetails taskFinancialDetails = new TaskFinancialDetails(new BigDecimal(115),false);
+        taskFinancialDetailsDao.save(taskFinancialDetails);
+        int id=taskFinancialDetails.getId();
+
+        //when
+        List<TaskFinancialDetails> resultList=taskFinancialDetailsDao.findByPaid(false);
+
+        //then
+        assertEquals(1, resultList.size());
+
+        //CleanUp
+        taskFinancialDetailsDao.deleteById(id);
+    }
+
+}
